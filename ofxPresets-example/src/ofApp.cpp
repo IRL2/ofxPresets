0f8d852f3e9fa6d992db76a3e584c6239f887861
luis toledo <luisernesto.toledo@gmail.com>
2025-04-02 20:07:36 +0200

    Update example

--- ofxPresets-example/src/ofApp.cpp
+++ ofxPresets-example/src/ofApp.cpp
@@ -3,54 +3,59 @@
 #include "ofxPresets.h"
 #include "ofxGui.h"
 
-struct Params : public ofxPresetsParametersBase {
-public:
-	ofParameter<int> x;
-	ofParameter<int> y;
-	ofParameter<int> radius;
-	ofParameter<ofColor> color;
+// use ofxPresetsParametersBase for a more flexible way of setting up a group of parameters
+// 
+//struct Params : public ofxPresetsParametersBase {
+//public:
+//	ofParameter<int> x;
+//	ofParameter<int> y;
+//	ofParameter<int> radius;
+//	ofParameter<ofColor> color;
+//
+//	Params() {
+//		groupName = "params";
+//
+//		parameterMap["x"] = &x.set("x pos", ofGetWidth() / 2);
+//		parameterMap["y"] = &y;
+//		parameterMap["color"] = &color;
+//		// radius is not added to the map, so it won't be saved
+//	}
+//};
+//Params p;
+//std::vector<ofxPresetsParametersBase*> allParameters;  // when using more than one group, wrap them on a vector
 
-	Params() {
-		groupName = "params";
+ofParameter<int> x;
+ofParameter<int> y;
+ofParameter<int> radius;
+ofParameter<ofColor> color;
 
-		parameterMap["x"] = &x.set("x pos", 0);
-		parameterMap["y"] = &y;
-		parameterMap["color"] = &color;
-		// radius is not added to the map, so it won't be saved
-	}
-};
-Params p;
-std::vector<ofxPresetsParametersBase*> allParameters;  // when using more than one group, wrap them on a vector
-
-ofParameterGroup pGroup;
+ofParameterGroup params;
 
 ofxPresets manager;
 
 ofxPanel gui;
+ofxPanel guiParams;
 ofxLabel currentPreset;
 ofxLabel instructions;
 ofxLabel internalSequence;
+ofxLabel playing;
 ofParameter<std::string> sequenceInput;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 	//ofSetLogLevel(OF_LOG_VERBOSE);
 
-    p.x.set("x", ofGetWidth() / 2, 0, ofGetWidth());
-    p.y.set("y", ofGetHeight() / 2, 0, ofGetHeight());
-    p.radius.set("radius", 50);
-    p.color.set("color", ofColor::white);
-
 	// Different ways of setting up the presets
 
 	// 1. using ofParameterGroup
-    pGroup.setName("params");
-	pGroup.add(p.x);
-	pGroup.add(p.y);
-	pGroup.add(p.color);
-    manager.setup(pGroup);
+    params.setName("params");
+	params.add(x.set("x", ofGetWidth() / 2, 0, ofGetWidth()));
+	params.add(y.set("y", ofGetHeight() / 2, 0, ofGetHeight()));
+	params.add(radius.set("radius", 5, 5, 80));
+	params.add(color.set("color", ofColor::white));
+    manager.setup(params);
 
-    // 2. Use the ofxPresetsParametersBase struct individually or in a group
+    // 2. Use the ofxPresetsParametersBase struct as defined above
 	//manager.setup(p);
 	// 
 	// or various in a vector
@@ -59,15 +64,23 @@ void ofApp::setup(){
 
 	ofAddListener(manager.transitionFinished, this, &ofApp::onPresetChanged);
 
-	gui.setup();
-	gui.setWidthElements(300);
-	gui.add(manager.interpolationDuration.set("Transition duration", 0.5f, 0.0f, 20.0f));
+	gui.setup("sequencer", ofxPanelDefaultFilename, 10, 10);
+	gui.setPosition(10, 0);
+	gui.setWidthElements(230);
+	gui.add(manager.interpolationDuration.set("Transition duration", 2.5f, 0.0f, 20.0f));
 	gui.add(manager.sequencePresetDuration.set("Preset duration", 0.5f, 0.0f, 20.0f));
-	gui.add(sequenceInput.set("Sequence", "1, 2*, ?-3, 8"));
-	gui.add(currentPreset.setup("Current preset", ""));
+	gui.add(sequenceInput.set("Sequence", "1, 2*, ?-2"));
 	gui.add(internalSequence.setup("Internal seq", ""));
+	gui.add(currentPreset.setup("Current preset", ""));
+    gui.add(playing.setup("Sequencer playing", ""));
 
-	gui.add(p.color.set("Color", ofColor::white));
+	guiParams.setup("params");
+	guiParams.setPosition(10, ofGetHeight()-260);
+	guiParams.setWidthElements(100);
+    guiParams.add(x);
+	guiParams.add(y);
+	guiParams.add(radius);
+	guiParams.add(color);
 }
 
 //--------------------------------------------------------------
@@ -75,15 +88,20 @@ void ofApp::update(){
 	manager.update();
 	currentPreset = ofToString(manager.getCurrentPreset());
 	internalSequence = ofToString(manager.sequence.get());
+    playing = ofToString(manager.isPlayingSequence() ? "true" : "false");
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofSetColor(p.color);
-	ofDrawCircle(p.x, p.y, p.radius);
-	gui.draw();
+	ofBackground(ofColor::gray);
+
+	ofSetColor(color);
+	ofDrawCircle(x, y, radius);
 
 	ofDrawBitmapStringHighlight("Press 1-9 to apply a preset\n<Shift> 1-9 to save into a preset\nS to start a sequence\nC to stop the sequence\nM to mutate", ofGetWidth() - 280, 38);
+
+	gui.draw();
+    guiParams.draw();
 }
 
 //--------------------------------------------------------------
@@ -118,59 +136,16 @@ void ofApp::onPresetChanged() {
 	ofLog() << "Recieving preset changed event";
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
 
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int _x, int _y, int button){
+	x = _x;
+	y = _y;
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::mousePressed(int _x, int _y, int button){
+	x = _x;
+	y = _y;
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-	p.x = x;
-	p.y = y;
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-	p.x = x;
-	p.y = y;
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
